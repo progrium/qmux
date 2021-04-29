3254d585a4a718df5a6c93b592d9c1e454e50272
Jeff Lindsay <progrium@gmail.com>
2021-04-29 16:00:58 -0500

    demos/groktunnel: misc improvements

--- demos/groktunnel/main.go
+++ demos/groktunnel/main.go
@@ -19,18 +19,13 @@ import (
 	"github.com/progrium/qmux/golang/session"
 )
 
-func fatal(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
 func main() {
 	var port = flag.String("p", "9999", "server port to use")
 	var host = flag.String("h", "vcap.me", "server hostname to use")
-
+	var addr = flag.String("b", "127.0.0.1", "ip to bind [server only]")
 	flag.Parse()
 
+	// client usage: groktunnel -h <server hostname> <local port>
 	if flag.Arg(0) != "" {
 		conn, err := net.Dial("tcp", "_new."+net.JoinHostPort(*host, *port))
 		fatal(err)
@@ -41,7 +36,7 @@ func main() {
 		client.Write(req)
 		resp, err := client.Read(req)
 		fatal(err)
-		fmt.Printf("port %s available at:\nhttp://%s\n", flag.Arg(0), resp.Header.Get("X-Public-Host"))
+		fmt.Printf("port %s http available at:\nhttp://%s\n", flag.Arg(0), resp.Header.Get("X-Public-Host"))
 		c, _ := client.Hijack()
 		sess := session.New(c)
 		for {
@@ -54,7 +49,8 @@ func main() {
 		return
 	}
 
-	l, err := net.Listen("tcp", net.JoinHostPort("0.0.0.0", *port))
+	// server usage: groktunnel -h <hostname> -b <bind ip>
+	l, err := net.Listen("tcp", net.JoinHostPort(*addr, *port))
 	fatal(err)
 	vmux, err := vhost.NewHTTPMuxer(l, 1*time.Second)
 	fatal(err)
@@ -62,10 +58,8 @@ func main() {
 		ml, err := vmux.Listen("_new." + net.JoinHostPort(*host, *port))
 		fatal(err)
 		mux := http.NewServeMux()
-		s := &http.Server{Handler: mux}
-
-		var sess *session.Session // TODO: make not a single global
-
+		srv := &http.Server{Handler: mux}
+		var sess *session.Session
 		mux.HandleFunc(fmt.Sprintf("_new.%s/", *host), func(w http.ResponseWriter, r *http.Request) {
 			publicHost := strings.ToLower(fmt.Sprintf("%s.%s", shortuuid.New(), *host))
 			pl, err := vmux.Listen(strings.TrimSuffix(net.JoinHostPort(publicHost, *port), ":80"))
@@ -86,7 +80,6 @@ func main() {
 					go join(conn, ch)
 				}
 			}()
-
 			w.Header().Add("X-Public-Host", strings.TrimSuffix(net.JoinHostPort(publicHost, *port), ":80"))
 			w.Header().Add("Connection", "close")
 			w.WriteHeader(http.StatusOK)
@@ -95,11 +88,10 @@ func main() {
 			log.Printf("%s: new session", publicHost)
 			sess.Wait()
 		})
-
-		s.Serve(ml)
+		srv.Serve(ml)
 	}()
 
-	log.Println("groktunnel ready")
+	log.Println("groktunnel server ready!")
 	for {
 		conn, err := vmux.NextError()
 		fmt.Println(err)
@@ -123,4 +115,10 @@ func join(a io.ReadWriteCloser, b io.ReadWriteCloser) {
 	wg.Wait()
 	a.Close()
 	b.Close()
+}
+
+func fatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
