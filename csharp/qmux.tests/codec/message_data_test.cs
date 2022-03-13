c45945f88877e382cff770f403bee4f567f656b5
Steven Murr <stevemurr@gmail.com>
2022-03-13 12:21:35 -0700

    port message_data with test

---
+++ csharp/qmux.tests/codec/message_data_test.cs
@@ -0,0 +1,26 @@
+namespace qmux.tests.codec;
+
+using Xunit;
+
+using qmux.codec;
+
+public class TestCodec
+{
+    [Fact]
+    public void Test_DataMessage_MarshalUnmarshalMuxRoundtrip()
+    {
+        var dataMessage = new DataMessage()
+        {
+            ChannelId = 10,
+            Length = 5,
+            Data = new byte[5] { 0x20, 0x20, 0x20, 0x20, 0x20 },
+        };
+
+        var marshaled = dataMessage.MarshalMux();
+        var dataMessage2 = new DataMessage();
+        dataMessage2.UnmarshalMux(marshaled);
+        Assert.Equal(dataMessage.ChannelId, dataMessage2.ChannelId);
+        Assert.Equal(dataMessage.Length, dataMessage2.Length);
+        Assert.Equal(dataMessage.Data, dataMessage2.Data);
+    }
+}
---
+++ csharp/qmux/codec/message.cs
@@ -0,0 +1,30 @@
+namespace qmux.codec;
+
+public enum MessageType : byte
+{
+    MessageChannelOpen = 100,
+    MessageChannelOpenConfirm = 101,
+    MessageChannelOpenFailure = 102,
+    MessageChannelWindowAdjust = 103,
+    MessageChannelData = 104,
+    MessageChannelEOF = 105,
+    MessageChannelClose = 106
+}
+
+public enum PayloadSizes : int
+{
+    MessageChannelOpen = 12,
+    MessageChannelOpenConfirm = 16,
+    MessageChannelOpenFailure = 4,
+    MessageChannelWindowAdjust = 8,
+    MessageChannelData = 8,
+    MessageChannelEOF = 4,
+    MessageChannelClose = 4
+
+}
+
+interface Message
+{
+    public (UInt32, bool) Channel();
+    public string String();
+}
