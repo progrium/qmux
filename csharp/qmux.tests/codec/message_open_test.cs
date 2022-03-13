a0b3253d19a61007c284f0dd1e282afcdffcfda9
Steven Murr <stevemurr@gmail.com>
2022-03-13 14:00:56 -0700

    add message_open and test

---
+++ csharp/qmux.tests/codec/message_open_test.cs
@@ -0,0 +1,26 @@
+namespace qmux.tests.codec;
+
+using Xunit;
+
+using qmux.codec;
+
+public class TestOpenMessage
+{
+    [Fact]
+    public void Test_OpenMessage_MarshalUnmarshalMuxRoundtrip()
+    {
+        var msg1 = new OpenMessage()
+        {
+            SenderId = 10,
+            WindowSize = 10,
+            MaxPacketSize = 10
+        };
+
+        var marshaled = msg1.MarshalMux();
+        var msg2 = new OpenMessage();
+        msg2.UnmarshalMux(marshaled);
+        Assert.Equal(msg1.SenderId, msg2.SenderId);
+        Assert.Equal(msg1.WindowSize, msg2.WindowSize);
+        Assert.Equal(msg1.MaxPacketSize, msg2.MaxPacketSize);
+    }
+}
---
+++ csharp/qmux/codec/message_open.cs
@@ -0,0 +1,50 @@
+namespace qmux.codec;
+
+using System.Text;
+public struct OpenMessage
+{
+    public UInt32 SenderId;
+    public UInt32 WindowSize;
+    public UInt32 MaxPacketSize;
+    public string String()
+    {
+        return $"OpenMessage SenderId:{this.SenderId} WindowSize:{this.WindowSize} MaxPacketSize:{this.MaxPacketSize}";
+    }
+    public (UInt32, bool) Channel()
+    {
+        return (0, false);
+    }
+    public byte[] MarshalMux()
+    {
+        // 12 + 1
+        var bufferSize = (int)PayloadSizes.MessageChannelOpen + 1;
+        var stream = new MemoryStream(new byte[bufferSize]);
+        using (var writer = new BinaryWriter(stream, Encoding.BigEndianUnicode, false))
+        {
+            // 0
+            writer.Write((byte)MessageType.MessageChannelOpen);
+            // 1-5
+            writer.Write(this.SenderId);
+            // 5-9
+            writer.Write(this.WindowSize);
+            // 10-14
+            writer.Write(this.MaxPacketSize);
+        }
+        return stream.ToArray();
+    }
+    public void UnmarshalMux(byte[] b)
+    {
+        var stream = new MemoryStream(b);
+        using (var reader = new BinaryReader(stream, Encoding.BigEndianUnicode))
+        {
+            // 0
+            reader.ReadByte();
+            // 1-5
+            this.SenderId = reader.ReadUInt32();
+            // 5-9
+            this.WindowSize = reader.ReadUInt32();
+            // 10-14
+            this.MaxPacketSize = reader.ReadUInt32();
+        }
+    }
+}
