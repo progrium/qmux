ffee2fb99e74cd904e2c323c41253f9678afe229
Steven Murr <stevemurr@gmail.com>
2022-03-20 12:09:42 -0700

    remove partial class experiment

--- csharp/qmux/mux/channel.cs
+++ csharp/qmux/mux/channel.cs
@@ -1,25 +1,24 @@
 namespace qmux.mux;
+
 using gostdlib.errors;
-public static partial class mux
+
+public interface IChannel
 {
-    public interface IChannel
-    {
-        // Read reads up to len(data) bytes from the channel.
-        public (int, errors.Error?) Read(byte[] data);
+    // Read reads up to len(data) bytes from the channel.
+    public (int, errors.Error?) Read(byte[] data);
 
-        // Write writes len(data) bytes to the channel.
-        public (int, errors.Error?) Write(byte[] data);
+    // Write writes len(data) bytes to the channel.
+    public (int, errors.Error?) Write(byte[] data);
 
-        // Close signals end of channel use. No data may be sent after this
-        // call.
-        public errors.Error? Close();
+    // Close signals end of channel use. No data may be sent after this
+    // call.
+    public errors.Error? Close();
 
-        // CloseWrite signals the end of sending data.
-        // The other side may still send data
-        public errors.Error? CloseWrite();
+    // CloseWrite signals the end of sending data.
+    // The other side may still send data
+    public errors.Error? CloseWrite();
 
-        // ID returns the unique identifier of this channel
-        // within the session
-        public UInt32 ID();
-    }
+    // ID returns the unique identifier of this channel
+    // within the session
+    public nuint ID();
 }
