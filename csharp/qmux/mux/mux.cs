3b579c3150736e299e8f42ac23dc079d274d354f
Steven Murr <stevemurr@gmail.com>
2022-03-20 12:09:08 -0700

    moved interface

--- csharp/qmux/mux/mux.cs
+++ csharp/qmux/mux/mux.cs
@@ -1,23 +1,7 @@
 namespace qmux.mux;
 
-using gostdlib.errors;
 
-public static partial class mux
+internal static class ErrorMessages
 {
-    internal static class ErrorMessages
-    {
-        public static string SessionDoesNotSupportWaiting = "session does not support waiting";
-    }
-    // Wait blocks until the session transport has shut down, and returns the
-    // error causing the shutdown.
-    public static errors.Error? Wait(ISession sess)
-    {
-        var w = sess as IWaiter;
-        if (w == null)
-        {
-            return new errors.Error(ErrorMessages.SessionDoesNotSupportWaiting);
-        }
-
-        return w.Wait();
-    }
+    public static string SessionDoesNotSupportWaiting = "session does not support waiting";
 }
